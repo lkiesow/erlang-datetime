3811e9013f3ca5d13749a94109e02656d5fd3315
Nathaniel Waisbrot <github.com@waisbrot.net>
2014-03-09 21:34:03 -0400

    Set up for Rebar
    Move the source file under 'src/', added an .app.src,
    added a stub rebar.config, added a Makefile, added
    a 'test' directory, added gitignore

---
+++ test/erlang_datetime_tests.erl
@@ -0,0 +1,36 @@
+%%% @doc
+%%% 
+%%% @end
+%%% Created :  9 Mar 2014 by Nathaniel Waisbrot <nathaniel@waisbrot.net>
+-module(erlang_datetime_tests).
+-author('nathaniel@waisbrot.net').
+
+-include_lib("eunit/include/eunit.hrl").
+
+rfc822_decode_test() ->
+    [
+     ?assertEqual({{2014,3,9},{1,11,0}}, datetime:datetime_decode("Sun, 9 Mar 14 01:11 GMT"))
+    ,?assertEqual({{2014,3,10},{21,0,0}}, datetime:datetime_decode("10 Mar 14 21:00 GMT"))
+    ,?assertEqual({{2011,1,1},{5,43,21}}, datetime:datetime_decode("1 Jan 11 05:43:21 GMT"))
+    ,?assertEqual({{2011,1,1},{9,43,21}}, datetime:datetime_decode("1 Jan 11 05:43:21 EDT"))
+    ].
+
+rfc822_encode_test() ->
+    [
+     ?assertEqual("28 Feb 00 10:29 +0000", datetime:datetime_encode({{2000,2,28},{10,29,0}}, 'GMT', rfc822))
+    ,?assertEqual("28 Feb 00 10:29 -0500", datetime:datetime_encode({{2000,2,28},{10,29,0}}, 'EST', rfc822))
+    ].
+
+rfc2822_decode_test() ->
+    [
+     ?assertEqual({{2014,3,9},{1,11,0}}, datetime:datetime_decode("Sat, 8 Mar 2014 01:11 GMT"))
+    ,?assertEqual({{2014,3,10},{21,0,0}}, datetime:datetime_decode("10 Mar 2014 21:00 GMT"))
+    ,?assertEqual({{2011,1,1},{5,43,21}}, datetime:datetime_decode("1 Jan 2011 05:43:21 GMT"))
+    ,?assertEqual({{2011,1,1},{9,43,21}}, datetime:datetime_decode("1 Jan 2011 05:43:21 EDT"))
+    ].
+
+rfc2822_encode_test() ->
+    [
+     ?assertEqual("28 Feb 2000 10:29 +0000", datetime:datetime_encode({{2000,2,28},{10,29,0}}, 'GMT', rfc822))
+    ,?assertEqual("28 Feb 2000 10:29 -0500", datetime:datetime_encode({{2000,2,28},{10,29,0}}, 'EST', rfc822))
+    ].
